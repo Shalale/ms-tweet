cb99fbc121080e68312b8e547f06e30bee6ebb18
Shalale <selale.memmedli11@gmail.com>
2024-01-11 17:40:11 +0400

    adding files

---
+++ src/main/java/com/example/ms/tweet/aspect/ExampleAspect.java
@@ -0,0 +1,26 @@
+package com.example.ms.tweet.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class ExampleAspect {
+
+    @Around("@annotation(LogExecutionTime)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+
+        Object proceed = joinPoint.proceed();
+
+        long executionTime = System.currentTimeMillis()-start;
+
+        log.info(joinPoint.getSignature() + " ExecutedTime is: " + executionTime);
+
+        return proceed;
+    }
+}
